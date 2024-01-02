4c756d1b99dcd5c2a24e310d7a814226a9f3471d
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-01-02 18:21:58 -0300

    mudando variavel de rota

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://lucky-itens-pesadelos-frontend-oz12pmkr4.vercel.app/',
+    origin: 'https://lucky-items-pesadelos-frontend.onrender.com/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
