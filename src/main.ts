2f5548186fa4b337820b886e9704c50c178a51a1
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-01-02 21:25:01 -0300

    rota config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://delightful-bombolone-8fe024.netlify.app/',
+    origin: 'https://delightful-bombolone-8fe024.netlify.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
