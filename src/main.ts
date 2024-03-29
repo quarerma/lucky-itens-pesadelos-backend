e271c8233f2481511c579ecae7d8295ae4068583
Gabriel Oliveira Quaresma <114524484+quarerma@users.noreply.github.com>
2024-03-29 13:05:59 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://main--sparkly-horse-d7a2c6.netlify.app',
+    origin: 'https://luck-itens.netlify.app',
     // origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
